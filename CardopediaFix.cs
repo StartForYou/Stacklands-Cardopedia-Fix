301691a21e164800a3546975253faedd2bdcd563
StartForYou <89837780+StartForYou@users.noreply.github.com>
2025-12-13 21:44:31 +0800

    Update files

--- CardopediaFix.cs
+++ CardopediaFix.cs
@@ -1,12 +1,29 @@
-﻿using HarmonyLib;
-using System;
-using System.Collections;
-using UnityEngine;
+﻿using CardopediaFix.Patches;
 
 namespace CardopediaFix
 {
     public class CardopediaFix : Mod
     {
+
+        public static CardopediaFix Instance;
+        public int mIdeaGroup;
+        public ModLogger logger;
+
+        public void Awake()
+        {
+            CardopediaFix.Instance = this;
+            this.logger = Logger;
+            //localization_ideagroup
+            //EnumHelper.GetName
+            //CardopediaScreen
+            //Villager villager = new Villager();
+            //Logger.Log("Id: ideagroup_" + customCardType.ToString().ToLower());
+            //SokLoc.instance.LoadTermsFromFile(System.IO.Path.Combine(this.Path, "localization_ideagroup.tsv"), false);
+            //SokLoc.instance.CurrentLanguage
+            this.Harmony.PatchAll(typeof(CombatStatsPatches));
+
+        }
+
         public override void Ready()
         {
             Logger.Log("Ready!");
---
+++ Patches/CombatStatsPatches.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+
+namespace CardopediaFix.Patches
+{
+    public class CombatStatsPatches
+    {
+        
+        //[HarmonyPatch(typeof(BaseVillager), "UpdateCardText")]
+        //[HarmonyPrefix]
+        //public static bool OnUpdateCardText(BaseVillager __instance)
+        //{
+        //    GameCard myGameCard = __instance.MyGameCard;
+        //    __instance.descriptionOverride = SokLoc.Translate(__instance.DescriptionTerm);
+        //    __instance.descriptionOverride = __instance.descriptionOverride + "\n\n<i>" + "Nothing.";
+        //    if (myGameCard != null && myGameCard.CardConnectorChildren.Count > 0 && __instance.MyGameCard.IsHovered)
+        //    {
+        //        __instance.descriptionOverride = SokLoc.Translate(__instance.DescriptionTerm);
+        //        __instance.descriptionOverride = __instance.descriptionOverride + "\n\n<i>" + "Nothing.";//this.GetConnectorInfoString(this.MyGameCard) + "</i>";
+        //    }
+        //    return false; // false: 拦截，true: 继续执行原方法
+        //}
+
+        [HarmonyPatch(typeof(CombatStats), "InitStats")]
+        [HarmonyPrefix]
+        public static bool OnInitStats(CombatStats __instance, CombatStats stats)
+        {
+            if (null == stats)
+            {
+                CardopediaFix.Instance.logger.Log("InitStats failed: stats error!");
+                __instance.HitChance = 0.5f; //by default
+                __instance.AttackSpeed = 3.5f; //by default
+                //__instance.MaxHealth = 1; // null
+                __instance.AttackDamage = 1; //by default
+                __instance.Defence = 1; //by default
+                //SpecialHit specialHit = new() { Chance = 0, HitType = SpecialHitType.None, Target = SpecialHitTarget.Target};
+                __instance.SpecialHits = []; // important
+                return false;
+            }
+            __instance.HitChance = stats.HitChance;
+            __instance.AttackSpeed = stats.AttackSpeed;
+            __instance.MaxHealth = stats.MaxHealth;
+            __instance.AttackDamage = stats.AttackDamage;
+            __instance.Defence = stats.Defence;
+            if (null != stats.SpecialHits)
+            {
+                //SpecialHit specialHit = new() { Chance = 0, HitType = SpecialHitType.None, Target = SpecialHitTarget.Target };
+                __instance.SpecialHits = [];
+            }
+            //GameCard myGameCard = __instance.MyGameCard;
+            //__instance.descriptionOverride = SokLoc.Translate(__instance.DescriptionTerm);
+            //__instance.descriptionOverride = __instance.descriptionOverride + "\n\n<i>" + "Nothing.";
+            //if (myGameCard != null && myGameCard.CardConnectorChildren.Count > 0 && __instance.MyGameCard.IsHovered)
+            //{
+            //    __instance.descriptionOverride = SokLoc.Translate(__instance.DescriptionTerm);
+            //    __instance.descriptionOverride = __instance.descriptionOverride + "\n\n<i>" + "Nothing.";//this.GetConnectorInfoString(this.MyGameCard) + "</i>";
+            //}
+            return false; // false: 拦截，true: 继续执行原方法
+        }
+    }
+}
